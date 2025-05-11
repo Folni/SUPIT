17cbf4429378e25c47202e19d011b3a9984bce24
Filip Folnegović <filip.folne@gmail.com>
2025-05-11 10:34:01 +0200

    HTML

--- components/SessionControl.js
+++
@@ -1,7 +0,0 @@
-import { sessionManager } from './managers/sessionManager.js';
-import { searchManager } from './managers/searchManager.js';
-
-$(document).ready(() => {
-    sessionManager.init();
-    searchManager.initialize();
-});
